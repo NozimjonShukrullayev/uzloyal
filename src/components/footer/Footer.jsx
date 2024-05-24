af598c867d639f89a8295b3297aa4e545361f8ee
NozimjonShukrullayev <nozimshukrullayev@gmail.com>
2024-05-25 00:19:17 +0500

    added footer width responsive

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,63 @@
-
+import { FaFacebookF } from "react-icons/fa";
+import { FaTelegramPlane } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import "./Footer.css";
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div id="footer">
+      <div className="container">
+        <div className="footer__grid">
+          <div className="footer__brand__info">
+            <h3 className="brand__title">LOYAL advokatlik byurosi</h3>
+            <p className="brand__description">Biz Sizga mukammal yordam bera oladigan eng yaxshi yuridik firmamiz!</p>
+            <div className="footer__social__media">
+              <span><FaFacebookF className="footer__social" /></span>
+              <span><FaTelegramPlane className="footer__social" /></span>
+              <span><FaInstagram className="footer__social" /></span>
+            </div>
+          </div>
+          <div className="footer__links">
+            <h4>Havolalar</h4>
+            <ul>
+              <li><Link className="footer__link" to={"/"}>Asosiy</Link></li>
+              <li><Link className="footer__link" to={"/about"}>Biz haqimizda</Link></li>
+              <li><Link className="footer__link" to={"/services"}>Xizmatlar</Link></li>
+              <li><Link className="footer__link" to={"/blog"}>Bizning maqolalar</Link></li>
+              <li><Link className="footer__link" to={"/news"}>Yangiliklar</Link></li>
+              <li><Link className="footer__link" to={"/resources"}>Hujjat Namunalari</Link></li>
+              <li><Link className="footer__link" to={"/faq"}>Savollarga javoblar</Link></li>
+              <li><Link className="footer__link" to={"/contact"}>Bog'lanish</Link></li>
+            </ul>
+          </div>
+          <div className="footer__services">
+            <h4>Xizmatlar</h4>
+            <ul>
+              <li><Link className="footer__link" to={"/contact"}>Oila huquqi</Link></li>
+              <li><Link className="footer__link" to={"/contact"}>Shaxsiy zarar</Link></li>
+              <li><Link className="footer__link" to={"/contact"}>Shaxsiy zarar</Link></li>
+              <li><Link className="footer__link" to={"/contact"}>Jinoyat huquqi</Link></li>
+              <li><Link className="footer__link" to={"/contact"}>Ta'lim huquqi</Link></li>
+              <li><Link className="footer__link" to={"/contact"}>Biznes huquqi</Link></li>
+              <li><Link className="footer__link" to={"/contact"}>Uy-joy huquqi</Link></li>   
+            </ul>
+          </div>
+        </div>
+        <hr />
+        <div className="footer__copyright__terms">
+          <div className="copyright">
+            <p>Copyright © 2023 Loyal.uz. All rights reserved.</p>
+          </div>
+          <div className="terms">
+            <Link className="terms__link" to={"/privacy"}>Privace & Policy</Link>
+            <Link className="terms__link" to={"/terms"}>Terms</Link>
+            <Link className="terms__link" to={"/about"}>About us</Link>
+            <Link className="terms__link" to={"/faq"}>FAQ</Link>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,27 +1,36 @@
 import { Link } from "react-router-dom";
+// import { useQuery } from "react-query";
 import { PiTelegramLogoBold } from "react-icons/pi";
 import { AiOutlineFacebook } from "react-icons/ai";
 import { GrInstagram } from "react-icons/gr";
 import logo from "../../assets/static/img/logo.png";
 import "./Header.css";
 
+// async function getServices() {
+//   // const response = await fetch("https://");
+// }
+
 function Header() {
+  // const { isLoading, error, data } = useQuery("services", () => getServices);
+
   return (
     <div id="header">
       <div className="container">
         <div className="header__grid">
           <div className="logo__box">
-            <img src={logo} alt="logo" />
+            <Link>
+              <img src={logo} alt="logo" />
+            </Link>
           </div>
           <div className="header__info__language">
             <div className="header__socials">
-              <a className="social__media" href="#">
+              <a className="social__media" href="https://t.me/uz_loyal" target={"_blank"}>
                 <PiTelegramLogoBold />
               </a>
-              <a className="social__media" href="#">
+              <a className="social__media" href="https://www.facebook.com/people/Advokat-uzbekistan-24/61554269997134/?mibextid=ZbWKwL" target={"_blank"}>
                 <AiOutlineFacebook />
               </a>
-              <a className="social__media" href="#">
+              <a className="social__media" href="https://www.instagram.com/advokat_uzbekistan_24/" target={"_blank"}>
                 <GrInstagram />
               </a>
               <a className="phone" href="tel:+998901319544">
@@ -33,14 +42,10 @@ function Header() {
             </div>
             <div className="languages">
               <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAApgAAAKYB3X3/OAAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAESSURBVEiJ7ZWxSsRQEEXPTF7MGlZYRVhYCzs/wcKf8VfEwsZqf0Kws/cLFDt/wCZiQNDVjVk2yRsL3YBdUjzExdvNK+69w+XOEzMjJDQo+1oIuNMrbgTGACqwt+2T/E2Xy5o2HKdI7TGnCEDt6RScQe4EDlfkx0cL7h4c2cvPxUapZ1Yq6YYRR8bzvNviAvtuNUxGnjgy7rP2icQZSQwiYPZlQqQTd4vWytNMEYGDcQPA1sBovFDVECmogqoR9UzNDcvXdri8hsluxU7tiT4ErQxVcAZpAXEDLoJh2V1A5mfTrJ+nfpCiKIJW+e8XLXyT/fT8MaSAnFwMgobs3jcXIfnXIWTglu9zHQC5/P/Jvy7wCe5MURzvEV3pAAAAAElFTkSuQmCC" alt="" />
-              <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAApgAAAKYB3X3/OAAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAESSURBVEiJ7ZWxSsRQEEXPTF7MGlZYRVhYCzs/wcKf8VfEwsZqf0Kws/cLFDt/wCZiQNDVjVk2yRsL3YBdUjzExdvNK+69w+XOEzMjJDQo+1oIuNMrbgTGACqwt+2T/E2Xy5o2HKdI7TGnCEDt6RScQe4EDlfkx0cL7h4c2cvPxUapZ1Yq6YYRR8bzvNviAvtuNUxGnjgy7rP2icQZSQwiYPZlQqQTd4vWytNMEYGDcQPA1sBovFDVECmogqoR9UzNDcvXdri8hsluxU7tiT4ErQxVcAZpAXEDLoJh2V1A5mfTrJ+nfpCiKIJW+e8XLXyT/fT8MaSAnFwMgobs3jcXIfnXIWTglu9zHQC5/P/Jvy7wCe5MURzvEV3pAAAAAElFTkSuQmCC" alt="" />
-              <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAApgAAAKYB3X3/OAAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAESSURBVEiJ7ZWxSsRQEEXPTF7MGlZYRVhYCzs/wcKf8VfEwsZqf0Kws/cLFDt/wCZiQNDVjVk2yRsL3YBdUjzExdvNK+69w+XOEzMjJDQo+1oIuNMrbgTGACqwt+2T/E2Xy5o2HKdI7TGnCEDt6RScQe4EDlfkx0cL7h4c2cvPxUapZ1Yq6YYRR8bzvNviAvtuNUxGnjgy7rP2icQZSQwiYPZlQqQTd4vWytNMEYGDcQPA1sBovFDVECmogqoR9UzNDcvXdri8hsluxU7tiT4ErQxVcAZpAXEDLoJh2V1A5mfTrJ+nfpCiKIJW+e8XLXyT/fT8MaSAnFwMgobs3jcXIfnXIWTglu9zHQC5/P/Jvy7wCe5MURzvEV3pAAAAAElFTkSuQmCC" alt="" />
-              <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAApgAAAKYB3X3/OAAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAESSURBVEiJ7ZWxSsRQEEXPTF7MGlZYRVhYCzs/wcKf8VfEwsZqf0Kws/cLFDt/wCZiQNDVjVk2yRsL3YBdUjzExdvNK+69w+XOEzMjJDQo+1oIuNMrbgTGACqwt+2T/E2Xy5o2HKdI7TGnCEDt6RScQe4EDlfkx0cL7h4c2cvPxUapZ1Yq6YYRR8bzvNviAvtuNUxGnjgy7rP2icQZSQwiYPZlQqQTd4vWytNMEYGDcQPA1sBovFDVECmogqoR9UzNDcvXdri8hsluxU7tiT4ErQxVcAZpAXEDLoJh2V1A5mfTrJ+nfpCiKIJW+e8XLXyT/fT8MaSAnFwMgobs3jcXIfnXIWTglu9zHQC5/P/Jvy7wCe5MURzvEV3pAAAAAElFTkSuQmCC" alt="" />
-              <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAApgAAAKYB3X3/OAAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAESSURBVEiJ7ZWxSsRQEEXPTF7MGlZYRVhYCzs/wcKf8VfEwsZqf0Kws/cLFDt/wCZiQNDVjVk2yRsL3YBdUjzExdvNK+69w+XOEzMjJDQo+1oIuNMrbgTGACqwt+2T/E2Xy5o2HKdI7TGnCEDt6RScQe4EDlfkx0cL7h4c2cvPxUapZ1Yq6YYRR8bzvNviAvtuNUxGnjgy7rP2icQZSQwiYPZlQqQTd4vWytNMEYGDcQPA1sBovFDVECmogqoR9UzNDcvXdri8hsluxU7tiT4ErQxVcAZpAXEDLoJh2V1A5mfTrJ+nfpCiKIJW+e8XLXyT/fT8MaSAnFwMgobs3jcXIfnXIWTglu9zHQC5/P/Jvy7wCe5MURzvEV3pAAAAAElFTkSuQmCC" alt="" />
-              {/* <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgA…XMnzqMCGF/F+xHunh6g+JRCqV8GnIrAAAAABJRU5ErkJggg==" alt="" />
-              <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgA…/JVgGraARC4xWLWlZL28owEz8wnD3LAAAAABJRU5ErkJggg==" alt="" />
-              <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgA…qAeMNBvXRomKYWzAKBh4AANS2DCY8GhUuAAAAAElFTkSuQmCC" alt="" />
-              <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgA…8FlALDP2iguYWjIKBBwAjgldkxe732wAAAABJRU5ErkJggg==" alt="" /> */}
+              <img src="https://cdn-icons-png.flaticon.com/128/555/555417.png" alt="" />
+              <img src="https://cdn-icons-png.flaticon.com/128/5111/5111624.png" alt="" />
+              <img src="https://cdn-icons-png.flaticon.com/128/555/555451.png" alt="" />
+              <img src="https://cdn-icons-png.flaticon.com/128/555/555560.png" alt="" />
             </div>
           </div>
           <div className="header__top__navbar">
@@ -53,10 +58,10 @@ function Header() {
         </div>
       </div>
       <div className="header__navbar">
-        <Link className="navbar__link" to={"/"}>YANGILIKLAR</Link>
-        <Link className="navbar__link" to={"/"}>YURIDIK KUTUBXONA</Link>
-        <Link className="navbar__link" to={"/"}>XIZMATLAR</Link>
-        <Link className="navbar__link" to={"/"}>HUJJAT NAMUNALARI</Link>
+        <Link className="navbar__link" to={"/news"}>YANGILIKLAR</Link>
+        <Link className="navbar__link" to={"/library"}>YURIDIK KUTUBXONA</Link>
+        <Link className="navbar__link" to={"/services"}>XIZMATLAR</Link>
+        <Link className="navbar__link" to={"/resources"}>HUJJAT NAMUNALARI</Link>
       </div>
     </div>
   )
